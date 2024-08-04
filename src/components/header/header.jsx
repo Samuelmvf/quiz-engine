56325a30a7f7af7e74854c834a1513aa3275f18d
Samuel Maia <samuelmvf@gmail.com>
2024-08-03 22:18:54 -0300

    feat: creation of the header component

---
+++ src/components/header/header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './header.scss';
+
+function Header() {
+  return (
+    <header className="header">
+      <img
+        className="header__logo"
+        src="../../../public/favicon.png"
+        alt="quiz-engine logo"
+      />
+      <h1 className="header__title">Quiz Engine</h1>
+      <p className="header__subtitle">
+        Welcome to our Quiz Engine, a powerful tool built with React and CSS/SCSS for rendering quiz questions dynamically based on a JSON structure.
+        Designed for versatility, it supports various question types, allowing for interactive and engaging assessments.
+      </p>
+    </header>
+  );
+}
+
+export default Header;
