e08ae075cd2872c7fba767c54a1f0a52f5ccd054
Samuel Maia <samuelmvf@gmail.com>
2024-08-06 20:50:22 -0300

    feat: new component for managing answers from one choice questions

---
+++ src/pages/quiz/components/answer/radio-button/answer-radio-button.jsx
@@ -0,0 +1,30 @@
+import { useRef } from "react";
+import { useFormContext } from "react-hook-form";
+
+import RadioButton from "@/components/radio-button/radio-button.jsx";
+
+const AnswerRadioButton = ({ options = [], questionId }) => {
+  const radioButtonRef = useRef(null);
+
+  const { watch, setValue, register } = useFormContext();
+  const questionAnswer = watch("answer");
+
+  return (
+    <>
+      {options.map(option => (
+        <RadioButton
+          ref={radioButtonRef}
+          key={option.id}
+          {...register("answer", { required: "This field is required. Please choose one." })}
+          name="answer"
+          label={option.label}
+          checked={questionAnswer === option.id}
+          value={option.id}
+          onChange={() => setValue("answer", option.id)}
+        />
+      ))}
+    </>
+  )
+}
+
+export default AnswerRadioButton;
