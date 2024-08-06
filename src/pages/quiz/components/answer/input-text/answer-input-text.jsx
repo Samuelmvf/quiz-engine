a50b6c3a938ebd2d8104c20b51aecc4a992f93ad
Samuel Maia <samuelmvf@gmail.com>
2024-08-06 20:50:53 -0300

    feat: new component for managing answers from input text questions

---
+++ src/pages/quiz/components/answer/input-text/answer-input-text.jsx
@@ -0,0 +1,20 @@
+import { useRef } from "react";
+import { useFormContext } from "react-hook-form";
+
+import TextArea from "@/components/text-area/text-area.jsx";
+
+const AnswerInputText = () => {
+  const { register } = useFormContext();
+  const textAreaRef = useRef(null);
+
+  return (
+    <TextArea
+      ref={textAreaRef}
+      {...register("answer", { required: true }) }
+      height="5rem"
+      placeholder="Type your answer..."
+    />
+  )
+}
+
+export default AnswerInputText;
