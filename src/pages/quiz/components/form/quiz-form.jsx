a4a2e264d474898a29f2dca95b45c37d467ee9a4
Samuel Maia <samuelmvf@gmail.com>
2024-08-06 20:54:06 -0300

    feat: new form component

---
+++ src/pages/quiz/components/form/quiz-form.jsx
@@ -0,0 +1,38 @@
+import { useForm, FormProvider } from "react-hook-form"
+
+import Button from "@/components/button/button.jsx";
+
+import QuizQuestion from "@/pages/quiz/components/question/quiz-question.jsx";
+
+import './quiz-form.scss'
+
+const QuizForm = ({ currentQuestion, answers, showPreviousButton, handleSubmitAnswer, handleBackForPreviousQuestion }) => {
+  const methods = useForm();
+
+  const { handleSubmit, formState: { errors}} = methods;
+  const { answer: answerError } = errors;
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(handleSubmitAnswer)} className="form">
+        {answerError && <p style={{ color: 'red'}}>{answerError.message}</p>}
+
+        <QuizQuestion
+          id={currentQuestion.id}
+          type={currentQuestion.type}
+          title={currentQuestion.title}
+          description={currentQuestion.description}
+          image={currentQuestion.image}
+          options={currentQuestion.options}
+        />
+
+        <footer className={`form__actions${showPreviousButton ? '' : '--single-button'}`}>
+          {showPreviousButton && <Button text="Previous Question" onClick={handleBackForPreviousQuestion} />}
+          <Button text="Next question" type="submit" />
+        </footer>
+      </form>
+    </FormProvider>
+  );
+}
+
+export default QuizForm;
