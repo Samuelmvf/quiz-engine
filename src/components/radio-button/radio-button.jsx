364a6a11fc4a839599560b82280d718d25ecb8af
Samuel Maia <samuelmvf@gmail.com>
2024-08-06 20:41:35 -0300

    refactor: radio-button component to use forwardRef

--- src/components/radio-button/radio-button.jsx
+++ src/components/radio-button/radio-button.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
+import { forwardRef } from 'react';
 import './radio-button.scss';
 
-const RadioButton = ({ label, name, checked, onChange }) => {
+const RadioButton = forwardRef(function({ label, name, checked, value, onChange, ...props }, ref) {
   return (
     <div className="radio">
       <label className="radio__label">
         <input
+          ref={ref}
           type="radio"
           name={name}
           checked={checked}
+          value={value}
           onChange={onChange}
           className="radio__input"
+          {...props}
         />
         <span className="radio__custom"></span>
         <span className="radio__text">{label}</span>
       </label>
     </div>
   );
-};
+});
 
 export default RadioButton;
